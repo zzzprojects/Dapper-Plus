e7a6e45919ee038d5878032545f354800fe6ee44
Jonathan Magnan <info@zzzprojects.com>
2023-11-28 11:22:55 -0500

    Remove source (were no longer public since 7 years ago)
    Remove source (were no longer public since 7 years ago)

--- src/Z.Dapper.Plus.NET40/DapperPlusEntityMapper/Field/Destination.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Z.Dapper.Plus
-{
-    public partial class DapperPlusEntityMapper
-    {
-        /// <summary>The destination table name.</summary>
-        internal string _table;
-    }
-}
